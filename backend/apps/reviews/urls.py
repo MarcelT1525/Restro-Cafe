61e0afb3b3d2d50c3a35443f7805e56a21cf1b5d
MarcelT1525 <marceltaylor25@gmail.com>
2022-11-23 14:36:12 -0500

    frontend

--- backend/apps/reviews/urls.py
+++
@@ -1,9 +0,0 @@
-from django import urls
-from django.urls import URLPattern, path
-from . import views
-
-
-urlpatterns = [
-    path('', views.ReviewList.as_view(), name='review_list'),
-    path('add/', views.ReviewAdd.as_view(), name='review_add')
-]
